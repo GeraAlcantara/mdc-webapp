511d4156f4ebcb958c91806364593471f0d49ad9
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-09-20 18:22:39 -0500

    adding text next to logo

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -7,10 +7,15 @@ function Header() {
   return (
     <header className='w-full  bg-black bg-opacity-80 py-2'>
       <div className='w-11/12 flex justify-between mx-auto  '>
-        <div>
+        <div className='flex items-center'>
             <Link href="/">
                 <Image className='object-contain cursor-pointer' src="/logo.png" alt="México developer center"  width={46} height={46}></Image>
             </Link>
+            <div className='flex flex-col'>
+              <h1 className='relative uppercase text-slate-50 font-RobotoCondense ml-3 opacity-60 text-lg font-normal leading-none'>Mexico</h1>
+              <span className='uppercase text-slate-50 font-RobotoCondense ml-3 opacity-60 text-lg font-light tracking-wide leading-none' >DEVELOPMENT CENTER</span>
+
+            </div>
         </div>
         <Nav/>
       </div>

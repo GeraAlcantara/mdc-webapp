22c5efc34f57002bbe49d3bcaff08345631d62b9
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-18 11:30:03 -0500

    Changes nav links styles

--- components/ui/NavLink.js
+++ components/ui/NavLink.js
@@ -1,21 +1,27 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import styles from '../../styles/NavLinks.module.css'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../../styles/NavLinks.module.css";
 
 /**
  * @param {Object} props - link properties
  * @param {string} [props.linkText] - texto del Link
  * @param {string} [props.slug] - Link
- * @returns 
+ * @returns
  */
-function NavLink({linkText = 'addtext', slug = 'addtext'}) {
+function NavLink({ linkText = "addtext", slug = "addtext" }) {
   const router = useRouter();
   return (
     <li className={`${styles.navbar__item} mx-4`}>
-        <Link href={slug.toLowerCase()} >
-            <a className={` font-normal text-gray-50 tracking-widest uppercase text-base ${router.pathname === '/'+slug || router.pathname === slug  ? styles.active : ''}` } >{linkText}</a>
-        </Link>
+      <Link href={slug.toLowerCase()}>
+        <a
+          className={` font-normal text-white tracking-widest capitalize text-base ${
+            router.pathname === "/" + slug || router.pathname === slug ? styles.active : ""
+          }`}
+        >
+          {linkText}
+        </a>
+      </Link>
     </li>
-  )
+  );
 }
-export default NavLink
+export default NavLink;

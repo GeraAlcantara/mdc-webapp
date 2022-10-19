f3c607c925975c157f93bf3cd3b0871976339562
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-19 13:56:03 -0500

    articles timeline

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Hero from "../components/ui/Hero";
-import Escudo from "../public/icons/escudo.svg";
+import Capacite from "../public/icons/capacite.svg";
 import ImgTimeline2 from "../public/timeline2.jpg";
 import ImgTimeline3 from "../public/timeline3.jpg";
 import ImgTimeline4 from "../public/timeline4.jpg";
 import ImgLaptop from "../public/laptop.jpg";
 import ImgTrofeo from "../public/trofeo.png";
 import Clients from "../components/ui/Clients";
+import { BsCheckLg } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -25,8 +26,8 @@ export default function Home() {
       <Clients />
       <div className=' md:text-center bg-white text-gray-900 py-4 lg:py-8 xl:py-12'>
         <div className=' p-10'>
-          <Escudo className='text-8xl m-auto mb-4' />
-          <h2 className='text-3xl font-bold uppercase pb-8 text-center'>Capacite a su personal</h2>
+          <Capacite className='text-8xl m-auto mb-4' />
+          <h2 className='text-3xl font-extrabold  pb-8 text-center'>Capacite a su personal</h2>
           <p className='max-w-prose m-auto pb-8'>
             Desde pequeñas empresas hasta empresas globales e instituciones públicas, los empleados son el activo más importante en cualquier organización, pero
             también son el punto débil en defensa de ciberseguridad. Podemos cambiar eso. Nuestras soluciones de capacitación sobre seguridad efectúan cambios
@@ -39,103 +40,109 @@ export default function Home() {
       {/* Section Timeline */}
       <section className='relative '>
         {/* timeline  */}
-        <div className='w-[2px] bg-[#4cafb8] h-full md:absolute left-1/2 translate-x-1/2 '></div>
-        <article className='bg-white text-gray-900'>
-          {/* section cards left */}
-          <div className='mdc-ui-container flex flex-col gap-4 md:gap-8 md:flex-row py-4 md:py-16'>
-            <div className='md:p-8 md:w-1/2 relative'>
+        <div className='flex justify-center'>
+          <div className='w-[2px] bg-gradient-to-b from-secondary to-accent bg-[#4cafb8] h-[75%] top-[12%] absolute hidden lg:block'></div>
+        </div>
+
+        {/* Cursos Multipremiados */}
+        <article className='bg-white text-gray-900  py-4 md:py-8'>
+          <div className='mdc-ui-container flex flex-col gap-4 md:gap-8 md:flex-row'>
+            <div className=' md:w-1/2 relative'>
               <Image src={ImgLaptop} alt='text alt '></Image>
               <div className='absolute top-[7%] left-[15%] w-[25%]'>
                 <Image src={ImgTrofeo} alt='text alt '></Image>
               </div>
             </div>
-            <div className='md:w-1/2 flex flex-col md:items-start md:justify-center py-4 md:p-8'>
-              <h2 className='font-bold text-3xl mb-8 uppercase '>CURSOS MULTIPREMIADOS</h2>
+            <div className=' justify-center items-center hidden lg:flex '>
+              <BsCheckLg className='relative p-2 text-lg box-content bg-secondary text-white rounded-full' />
+            </div>
+
+            <div className='md:w-1/2 flex flex-col md:items-start md:justify-center'>
+              <h2 className='font-bold text-4xl mb-8 text-secondary '>Cursos Multipremiados</h2>
               <p className='max-w-prose md:text-left mb-8'>
                 Nuestros productos han tenido reconocimiento internacional ganando más de 40 premios como MarCom Awards, Davey Awards, W³ Awards, Golden Bridge
                 Awards, Summit Creative Awards, Network Products Guide IT World Awards, Omni Awards, Interactive Media Awards.
               </p>
-              {/* Link style as Button */}
               <Link href='#'>
-                <a className='place-self-start py-2 px-10 bg-gradient-to-r from-cyan-500 to-secondary rounded-full text-white uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
+                <a className='place-self-start py-2 px-10 bg-secondary rounded-full text-white uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
                   <span>Casos de exito</span>
                 </a>
               </Link>
             </div>
           </div>
-          {/* END section cards left */}
         </article>
-        <article>
-          {/* section cards right */}
-          <div className='mdc-ui-container flex flex-col-reverse gap-4 md:gap-8 md:flex-row py-4 md:py-16'>
-            <div className='md:w-1/2 flex flex-col md:items-end md:justify-center py-4 md:p-8'>
-              <h2 className='font-bold text-3xl mb-8 uppercase'>INTERACTIVIDAD</h2>
+        {/* Interactividad */}
+        <article className='py-4 md:py-8'>
+          <div className='mdc-ui-container flex flex-col-reverse gap-4 md:gap-8 md:flex-row '>
+            <div className='md:w-1/2 flex flex-col md:items-end md:justify-center py-4 '>
+              <h2 className='font-bold text-4xl mb-8 text-[#00bec6]'>Interactividad</h2>
               <p className='max-w-prose md:text-right mb-8'>
                 En México DC lo visual es tan importante como el contenido, diseñamos cursos con video y animaciones personalizadas para impulsar el
                 aprendizaje. Este tipo de factores generan entusiasmo y aumenta el interes en sus empleados a la hora de tomar un curso.
               </p>
-
-              {/* Link style as Button */}
               <Link href='#'>
-                <a className='place-self-start md:place-self-auto py-2 px-10 bg-gradient-to-r from-cyan-500 to-secondary rounded-full text-white uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
+                <a className='place-self-start md:place-self-auto py-2 px-10 bg-[#00bec6] rounded-full text-bg_primary uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
                   <span>Ver demos</span>
                 </a>
               </Link>
             </div>
-            <div className='md:p-8 md:w-1/2'>
+            <div className=' justify-center items-center hidden lg:flex '>
+              <BsCheckLg className='relative p-2 text-lg box-content bg-[#00bec6] text-white rounded-full' />
+            </div>
+            <div className='md:w-1/2'>
               <Image src={ImgTimeline2} alt='text alt '></Image>
             </div>
           </div>
-          {/* End section cards right */}
         </article>
-        <article>
-          {/* section cards left */}
-          <div className='mdc-ui-container flex flex-col gap-4 md:gap-8 md:flex-row py-4 md:py-16'>
-            <div className='md:p-8 md:w-1/2'>
+        {/* Mobile Ready */}
+        <article className='py-4 md:py-8'>
+          <div className='mdc-ui-container flex flex-col gap-4 md:gap-8 md:flex-row '>
+            <div className='md:w-1/2'>
               <Image src={ImgTimeline3} alt='text alt '></Image>
             </div>
-            <div className='md:w-1/2 flex flex-col md:items-start md:justify-center py-4 md:p-8'>
-              <h2 className='font-bold text-3xl mb-8 uppercase '>MOBILE READY</h2>
+            <div className=' justify-center items-center hidden lg:flex '>
+              <BsCheckLg className='relative p-2 text-lg box-content bg-[#4cff91] text-white rounded-full' />
+            </div>
+            <div className='md:w-1/2 flex flex-col md:place-self-auto md:justify-center py-4 '>
+              <h2 className='font-bold text-4xl mb-8 text-[#4cff91] '>Mobile Ready</h2>
               <p className='max-w-prose md:text-left mb-8'>
                 Creamos cursos fácilmente accesibles en tabletas o dispositivos móviles. Estos materiales no solo son aptos para dispositivos móviles. Cada
                 aspecto de la capacitación se diseñó cuidadosamente, desde el contenido hasta la pantalla, para proporcionar los materiales más efectivos y de
                 fácil acceso.
               </p>
-              {/* Link style as Button */}
               <Link href='#'>
-                <a className='place-self-start py-2 px-10 bg-gradient-to-r from-cyan-500 to-secondary rounded-full text-white uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
+                <a className='place-self-start py-2 px-10 bg-[#4cff91] rounded-full text-bg_primary uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
                   <span>Catalogos de cursos</span>
                 </a>
               </Link>
             </div>
           </div>
-          {/* END section cards left */}
+          {/* End section cards right */}
         </article>
-        <article className='bg-bg_primary'>
-          {/* section cards right */}
-          <div className='mdc-ui-container flex flex-col-reverse gap-4 md:gap-8 md:flex-row py-4 md:py-16'>
-            <div className='md:w-1/2 flex flex-col md:items-end md:justify-center py-4 md:p-8'>
-              <h2 className='font-bold text-3xl mb-8 uppercase md:text-right '>Plataforma LMS Intuitiva</h2>
+        {/* Plataforma LMS Intuitiva */}
+        <article className='py-4 md:py-8'>
+          <div className='mdc-ui-container flex flex-col-reverse gap-4 md:gap-8 md:flex-row '>
+            <div className='md:w-1/2 flex flex-col md:items-end md:justify-center py-4 '>
+              <h2 className='font-bold text-4xl mb-8 text-[#02e239] md:text-right xl:w-1/2'>Plataforma LMS Intuitiva</h2>
               <p className='max-w-prose md:text-right mb-8'>
                 Nuestros productos han tenido reconocimiento internacional ganando más de 40 premios como MarCom Awards, Davey Awards, W³ Awards, Golden Bridge
                 Awards, Summit Creative Awards, Network Products Guide IT World Awards, Omni Awards, Interactive Media Awards.
               </p>
-
-              {/* Link style as Button */}
               <Link href='#'>
-                <a className='place-self-start md:place-self-auto py-2 px-10 bg-gradient-to-r from-cyan-500 to-secondary rounded-full text-white uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
+                <a className='place-self-start md:place-self-auto py-2 px-10 bg-[#02e239] text-bg_primary rounded-full uppercase hover:from-cyan-500 hover:to-blue-600 transition-all '>
                   <span>Planes y precios</span>
                 </a>
               </Link>
             </div>
-            <div className='md:p-8 md:w-1/2'>
+            <div className=' justify-center items-center hidden lg:flex '>
+              <BsCheckLg className='relative p-2 text-lg box-content bg-[#00bec6] text-white rounded-full' />
+            </div>
+            <div className='md:w-1/2'>
               <Image src={ImgTimeline4} alt='text alt '></Image>
             </div>
           </div>
-          {/* End section cards right */}
         </article>
       </section>
-      {/* END Section Timeline */}
     </>
   );
 }

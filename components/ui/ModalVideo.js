cc25d2a3a79eeb86651ef92efe39474acca6031f
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-20 14:48:47 -0500

    increase opacity on modal

--- components/ui/ModalVideo.js
+++ components/ui/ModalVideo.js
@@ -38,7 +38,7 @@ export default function ModalVideo() {
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <div className='fixed inset-0 bg-black bg-opacity-25' />
+            <div className='fixed inset-0 bg-black bg-opacity-80' />
           </Transition.Child>
 
           <div className='fixed inset-0 bg-black/40 overflow-y-auto'>

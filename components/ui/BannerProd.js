71adc5d8295c34b2b8b2ebd91fa148e26efbbaaa
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-10 18:06:18 -0500

    dividers and parralax prods

--- components/ui/BannerProd.js
+++ components/ui/BannerProd.js
@@ -1,27 +1,25 @@
 import Image from "next/image";
-import ImgBanner from "../../public/banner.jpg";
+import ImgBannerProd from "../../public/bannerProds.jpg";
 import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 import Link from "next/link";
 
-function BannerParallax() {
+function BannerProd() {
   return (
     <ParallaxBanner className='relative w-full xl:h-[800px] aspect-square lg:aspect-video'>
       <ParallaxBannerLayer speed={-20}>
-        <Image src={ImgBanner} objectFit='cover' layout='fill' alt='employee'></Image>
+        <Image src={ImgBannerProd} objectFit='cover' layout='fill' alt='grupo de personas conversando frente a un ordenador'></Image>
       </ParallaxBannerLayer>
       <ParallaxBannerLayer>
         <div className='absolute flex justify-end h-full w-full xl:m-auto 2xl:w-[90%]'>
           <div className='flex justify-end relative top-0 items-center xl:items-start xl:top-32 w-3/4 md:w-1/2 xl:w-1/3 p-5 lg:p-10 '>
             <div className='text-right'>
-              <h1 className='text-xl md:text-4xl'>Cursos a partir de</h1>
-              <h2 className='font-extrabold text-4xl md:text-7xl text-accent '>$60 pesos</h2>
-              <p className='font-extrabold text-3xl md:text-5xl '>por usuario</p>
-              <span className='font-light md:text-lg '>- POR CADA CURSO -</span>
+              <h2 className='text-xl md:text-4xl'>Agenda una</h2>
+              <h2 className='font-extrabold text-4xl md:text-7xl text-white '>Reunión</h2>
               <Link href='#'>
                 <div className='flex justify-end rounded-lg pt-6 hover:cursor-pointer'>
-                  <div className='flex rounded-full bg-secondary px-4 py-2 md:px-12 md:py-4 align-middle leading-none'>
-                    <span>Planes y precios</span>
-                  </div>
+                  <a className='flex rounded-full bg-accent text-gray-900 px-4 py-2 md:px-12 md:py-4 align-middle leading-none uppercase font-bold '>
+                    <span className='select-none'>Contactanos</span>
+                  </a>
                 </div>
               </Link>
             </div>
@@ -32,4 +30,4 @@ function BannerParallax() {
   );
 }
 
-export default BannerParallax;
+export default BannerProd;
--- pages/productos/index.js
+++ pages/productos/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Employee from "../../public/employeetablet.png";
 import CheckMiddleImg from "../../public/heroProductosCheckbackMiddle.png";
 import CheckBackImg from "../../public/heroProductosCheckback.png";
 import CheckFrontImg from "../../public/heroProductosCheckbackFront.png";
@@ -9,6 +8,9 @@ import ProdHeroGirlImg from "../../public/heroProductosGirl.png";
 import { RiUserFill } from "react-icons/ri";
 import ModalDemoReel from "../../components/ui/ModalDemoReel";
 import TabsCarousel from "../../components/ui/TabsCourses";
+import BoxOpen from "../../public/icons/boxOpen.png";
+import ImgShopingCard from "../../public/icons/shoppingCard.png";
+import BannerProd from "../../components/ui/BannerProd";
 
 function Productos() {
   return (
@@ -23,7 +25,7 @@ function Productos() {
         <title>Mexico Development Center | Productos</title>
       </Head>
       <div className='pt-[68px]  '>
-        <div className='p-4 md:p-6 lg:p-10 mb-20 flex flex-col-reverse lg:flex-row lg:gap-8 bg-gradient-to-b from-[#1c3d5c] via-[#1c3d5c]/30 to-bg_primary rounded-t-3xl mdc-ui-container min-h-[calc(100vh-70px)] lg:min-h-0 justify-around lg:items-center '>
+        <div className='p-4 md:p-6 lg:p-10 container mb-20 flex flex-col-reverse lg:flex-row lg:gap-8 bg-gradient-to-b from-[#1c3d5c] via-[#1c3d5c]/30 to-bg_primary rounded-t-3xl mdc-ui-container min-h-[calc(100vh-70px)] lg:min-h-0 justify-around lg:items-center '>
           <div className='lg:w-1/2'>
             <h1 className='text-5xl lg:text-8xl text-accent font-extrabold'>
               <small className='text-white text-2xl lg:text-6xl font-bold'>Librería de</small>
@@ -38,7 +40,7 @@ function Productos() {
             </div>
           </div>
           <div className='relative w-full xl:w-1/2'>
-            <div className='relative top-0 left-0 grid grid-cols-7 grid-rows-3   '>
+            <div className='relative top-0 left-0 grid grid-cols-7 grid-rows-3'>
               <div className='row-start-1 col-start-3 p-2 opacity-30 flex items-end'>
                 <Image loading='eager' src={CheckBackImg} alt='icono de marca de verificacion ' />
               </div>
@@ -57,6 +59,15 @@ function Productos() {
             </div>
           </div>
         </div>
+        {/* Divider */}
+        <div className='flex mdc-ui-container justify-center items-end m-auto my-16 '>
+          <div className='bg-brandBlue-400 h-[1px] w-1/2 flex-1'></div>
+          <div className='mx-4 relative w-20 h-20'>
+            <Image src={BoxOpen} width={171} layout='responsive' height={180} alt='icono de caja abierta' />
+          </div>
+          <div className='bg-brandBlue-400 h-[1px] flex-1'></div>
+        </div>
+        {/* Divider END */}
         {/* Section Libreria  */}
         <section className='lg:m-10'>
           <header>
@@ -69,8 +80,21 @@ function Productos() {
           <TabsCarousel />
           <div className='flex flex-col lg:flex-row gap-8'></div>
         </section>
+        {/* parallaxBaner */}
+        <div className='py-16 xl:py-24'>
+          <BannerProd />
+        </div>
+        {/* Divider */}
+        <div className='flex mdc-ui-container justify-center items-end m-auto xl:my-16 '>
+          <div className='bg-brandBlue-400 h-[1px] w-1/2 flex-1'></div>
+          <div className='mx-4 relative w-20 h-20'>
+            <Image src={ImgShopingCard} width={171} layout='responsive' height={180} alt='icono de caja abierta' />
+          </div>
+          <div className='bg-brandBlue-400 h-[1px] flex-1'></div>
+        </div>
+        {/* Divider END */}
         {/* Card */}
-        <div className=' mb-10 '>
+        <div className=' mb-10 mdc-ui-container '>
           <div className='borderAnimated  '>
             <i className='opacity-0 transition-all'></i>
             <div className='p-6 border-cyan-800/20 rounded-xl border-[1px] flex flex-col gap-4 relative'>

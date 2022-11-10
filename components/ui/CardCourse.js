7d71317220935170a2d9c8f06711a97b185bc985
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-10 16:30:02 -0500

    adding hover effect cards

--- components/ui/CardCourse.js
+++ components/ui/CardCourse.js
@@ -11,9 +11,17 @@ import Link from "next/link";
 function CardsCarousel({ coursename, numtopics, thumbnail, thumbnailAlt, numlessons, duration, slug }) {
   return (
     <Link href={slug}>
-      <div className='flex flex-col w-[250px] md:w-[300px] lg:w-[250px] m-auto xl:w-[300px] h-[450px] rounded-2xl bg-brandBlue-500 overflow-hidden justify-between '>
-        <div className='min-h-[230px] w-full relative'>
-          <Image src={thumbnail} layout='fill' objectFit='cover' placeholder='blur' loading='lazy' alt={thumbnailAlt} />
+      <div className='group flex flex-col w-[250px] md:w-[300px] lg:w-[250px] m-auto xl:w-[300px] h-[450px] rounded-2xl bg-brandBlue-500 hover:bg-brandBlue-600 overflow-hidden justify-between hover:cursor-pointer '>
+        <div className='min-h-[230px] w-full relative  '>
+          <Image
+            className='group-hover:scale-110 transition-all duration-300 ease-out'
+            src={thumbnail}
+            layout='fill'
+            objectFit='cover'
+            placeholder='blur'
+            loading='lazy'
+            alt={thumbnailAlt}
+          />
         </div>
         <div className='pt-6 px-6 '>
           <h3 className='text-xl text-gray-100'>{coursename}</h3>

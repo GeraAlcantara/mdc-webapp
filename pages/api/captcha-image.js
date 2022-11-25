8917caefda7fccc5a5f59ef8cb36edb4bc6e6470
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-25 00:30:04 -0500

    no cookieOptions

--- pages/api/captcha-image.js
+++ pages/api/captcha-image.js
@@ -29,8 +29,5 @@ export default withIronSessionApiRoute(
   {
     cookieName: "MDC_SESSION",
     password: process.env.SESSION_SECRET,
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    },
   }
 );
--- pages/contactanos.js
+++ pages/contactanos.js
@@ -72,8 +72,5 @@ export const getServerSideProps = withIronSessionSsr(
   {
     cookieName: "MDC_SESSION",
     password: process.env.SESSION_SECRET,
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    },
   }
 );

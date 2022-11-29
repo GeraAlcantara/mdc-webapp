2809c74333919ec180ddf36e996af842024a5aba
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-29 12:15:53 -0500

    cambio de svg

--- pages/contactanos.js
+++ pages/contactanos.js
@@ -3,7 +3,7 @@ import { withIronSessionSsr } from "iron-session/next";
 import Address from "../components/ui/Address";
 import CaptchaForm from "../components/ui/CaptchaForm";
 import FormContact from "../components/ui/FormContact";
-import IconShopingCard from "../public/icons/planePrecios.svg";
+import IconContacto from "../public/icons/contacto.svg";
 import { newCaptchaImages } from "./api/captcha-image";
 
 export default function contactanos() {
@@ -25,7 +25,7 @@ export default function contactanos() {
         <div className='flex mdc-ui-container justify-center items-end m-auto xl:my-16 my-8 '>
           <div className='bg-brandBlue-400 h-[1px] w-1/2 flex-1'></div>
           <div className='mx-4 relative'>
-            <IconShopingCard className='w-20 h-auto' />
+            <IconContacto className='w-20 h-auto' />
           </div>
           <div className='bg-brandBlue-400 h-[1px] flex-1'></div>
         </div>

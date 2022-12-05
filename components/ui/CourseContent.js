4e10a6cb63f40f0a75df7c8b716c710b05480af2
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-12-05 14:17:25 -0500

    overflow hidden lessons

--- components/ui/CourseContent.js
+++ components/ui/CourseContent.js
@@ -17,16 +17,17 @@ function CourseContent({
   ],
 }) {
   return (
-    <section className='mt-8'>
-      <div className='flex flex-col mb-2 space-y-4'>
-        <h2 className='text-3xl font-bold'>Course Content</h2>
+    <section className='mt-8 h-full flex flex-col bg-brandBlue-500  rounded-xl '>
+      <div className='flex flex-col mb-2 space-y-2 p-4'>
+        <h2 className='text-2xl lg:text-3xl font-bold'>Course Content</h2>
         <div className='text-sm font-normal text-gray-400'>
           <p>
-            {duration}m • {numLessons} lessons
+            {duration} min • {numLessons} Lessons
           </p>
         </div>
       </div>
-      <ul>
+      <hr />
+      <ul className='p-4 md:max-h-[700px] lg:max-h-[590px] md:pr-4 lg:pr-6 overflow-y-scroll '>
         {lessons.map((lesson, idx) => (
           <li key={idx + lesson.title}>
             <div className='flex py-2 font-semibold leading-tight '>

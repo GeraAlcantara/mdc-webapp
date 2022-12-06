faa02ec2650ffa31dc232c3f9f49940741563b72
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-12-06 12:29:13 -0500

    adding description meta data generated by AI

--- components/data/DataHeader.js
+++ components/data/DataHeader.js
@@ -15,34 +15,39 @@ export const DataHeadProd = {
   previewImage: "/socialCards/libreria.jpg",
   previewImageAlt: "Mexico development center",
   pageTitle: "MDC | Libreria de cursos",
-  pageDescription: "Descripcion Libreria de cursos",
+  pageDescription:
+    "Nuestro catalogo de cursos de capacitación en línea (eLearning) compatibles con los LMS. Nuestros cursos son elaborados con el objetivo de brindarle a tus colaboradores una formación que te permita mejorar la productividad y la eficiencia de tu empresa. Ofrecemos una amplia gama de cursos en línea, desde temas básicos hasta cursos avanzados, para satisfacer las necesidades de todos los niveles de conocimiento. Nuestros cursos son diseñados para adaptarse a los estándares de los principales LMS, como Chamilo, Moodle, etc. Además, nuestros cursos se pueden personalizar para satisfacer las necesidades específicas de tu empresa. Si estás buscando una solución de capacitación en línea para tu empresa, Mexico Development Center es la mejor opción.",
   slug: "/libreria",
 };
 export const DataHeadServicios = {
   previewImage: "/socialCards/servicios.jpg",
   previewImageAlt: "Mexico development center",
   pageTitle: "MDC | Servicios",
-  pageDescription: "Descripcion Pagina Servicios",
+  pageDescription:
+    "¿Estás buscando soluciones de capacitación y entrenamiento para tu empresa? Producciones A Tu Medida ofrece soluciones de capacitación y entrenamiento online para ayudar a tu empresa a alcanzar sus objetivos. Ofrecemos servicios de traducción y localización para adaptar tus contenidos a los mercados internacionales. Nuestra amplia red de socios estratégicos nos permite ofrecer soluciones a medida para satisfacer tus necesidades. ¡Descubre cómo podemos ayudarte hoy!",
   slug: "/servicios",
 };
 export const DataHeadContact = {
   previewImage: "/socialCards/contacto.jpg",
   previewImageAlt: "Mexico development center",
   pageTitle: "MDC | Contáctanos",
-  pageDescription: "Descripcion Pagina Contáctanos",
+  pageDescription:
+    "Bienvenido a nuestra página de contacto. Aquí puedes encontrar toda la información necesaria para ponerte en contacto con nosotros. Nuestro equipo de atención al cliente está disponible para responder a tus preguntas y proporcionarte una solución rápida y eficiente. Si tienes alguna pregunta, comentario o inquietud, no dudes en contactarnos. Estamos encantados de ayudarte a encontrar la mejor solución para tus necesidade",
   slug: "/contacto",
 };
 export const DataHeadGracias = {
   previewImage: "/socialCards/homeSocialCard.jpg",
   previewImageAlt: "Mexico development center",
   pageTitle: "MDC | Gracias por contáctanos",
-  pageDescription: "Descripcion Gracias por contáctanos",
+  pageDescription:
+    "Estamos encantados de que nos hayas contactado y queremos darte las gracias por haberlo hecho. Estamos comprometidos a ofrecerte la mejor atención posible. Si necesitas ayuda adicional, no dudes en contactarnos de nuevo. ¡Gracias de nuevo por contactarnos!",
   slug: "/gracias",
 };
 export const DataHeadBlog = {
   pageDescription: "Descripcion del Blog",
   pageTitle: "MDC | Noticias y Artículos",
   previewImage: "/socialCards/homeSocialCard.jpg",
-  previewImageAlt: "Mexico development center",
+  previewImageAlt:
+    "Bienvenidos al Blog de Mexico development center (MDC), el lugar perfecto para mantenerse al día con todas las novedades y tendencias en el mundo de la capacitación de colaboradores y los sistemas de gestión de aprendizaje. Aquí encontrarás artículos sobre los últimos avances en LMS, consejos y recomendaciones para mejorar la capacitación de tus colaboradores, información sobre los como crear los mejores cursos en línea y mucho más. Estamos comprometidos a proporcionar contenido de calidad para ayudarte a mejorar la formación de tus colaboradores y aprovechar al máximo los sistemas de gestión de aprendizaje. ¡Sigue con nosotros para mantenerte al día con todas las novedades en el mundo de la capacitación de colaboradores y los LMS!",
   slug: "/blog",
 };

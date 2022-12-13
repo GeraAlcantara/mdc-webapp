781ae360a61e133489e779a157b2d72cd20e3240
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-12-13 15:42:25 -0500

    Change Timeline to TS

---
+++ components/ui/Timeline.tsx
@@ -0,0 +1,11 @@
+import { useParallax } from "react-scroll-parallax";
+
+function Timeline() {
+  const { ref } = useParallax<HTMLDivElement>({
+    scaleY: [0, 1, "easeInOutQuad"],
+  });
+
+  return <div ref={ref} className='origin-top w-[2px] bg-gradient-to-b from-secondary to-accent bg-[#4cafb8] h-[78%] top-[12%] absolute hidden lg:block'></div>;
+}
+
+export default Timeline;
